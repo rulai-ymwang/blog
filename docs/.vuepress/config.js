620f7e9c73bd52019bfe6e85ea901cb3ac69f9c1
Yaomiao Wang <ym.wang@cn.rul.ai>
2019-04-26 18:35:55 +0800

    Update docs.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,10 @@
 module.exports = {
   base: '/blog/',
-  title: "Ninja.YM's Blog",
+  // title: "Ninja.YM's Blog",
+  themeConfig: {
+    logo: 'logo.png',
+  },
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   markdown: {
     lineNumbers: true,
   },
